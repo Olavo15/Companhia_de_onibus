0bb16ec98f5f8a4eca7a6e1a9dd6cb9e5a0e5a4e
¨LucasPierreAraujo¨ <pedrolucaspierre@gmail.com>
2024-04-10 16:09:16 -0300

    ´att´

--- passagem.php
+++ passagem.php
@@ -51,35 +51,36 @@ class PassageiroRepository {
         $id = $passageiro->getId();
         $nome = $passageiro->getNome();
         $idade = $passageiro->getIdade();
-
+    }   
+}
         // Prepare a consulta SQL para inserir o passageiro no banco de dados
-        $sql = "INSERT INTO passageiros (id, nome, idade) VALUES ('$id', '$nome', '$idade')";
+//         $sql = "INSERT INTO passageiros (id, nome, idade) VALUES ('$id', '$nome', '$idade')";
 
-        // Execute a consulta
-        if ($this->conexao->query($sql) === TRUE) {
-            echo "Passageiro salvo no banco de dados com sucesso.\n";
-        } else {
-            echo "Erro ao salvar passageiro no banco de dados: " . $this->conexao->error;
-        }
-    }
-}
-
-
-$conexao = new mysqli("localhost", "usuario", "senha", "nome_do_banco");
-if ($conexao->connect_error) {
-    die("Erro de conexão: " . $conexao->connect_error);
-}
-
-$passageiroRepository = new PassageiroRepository($conexao);
-
-$passageiro1 = new Passageiro('João', 30);
-$passageiro2 = new Passageiro('Maria', 25);
+//         // Execute a consulta
+//         if ($this->conexao->query($sql) === TRUE) {
+//             echo "Passageiro salvo no banco de dados com sucesso.\n";
+//         } else {
+//             echo "Erro ao salvar passageiro no banco de dados: " . $this->conexao->error;
+//         }
+//     }
+// }
 
 
-$passageiroRepository->salvar($passageiro1);
-$passageiroRepository->salvar($passageiro2);
+// $conexao = new mysqli("localhost", "usuario", "senha", "nome_do_banco");
+// if ($conexao->connect_error) {
+//     die("Erro de conexão: " . $conexao->connect_error);
+// }
 
-// Fechando a conexão com o banco de dados
-$conexao->close();
+// $passageiroRepository = new PassageiroRepository($conexao);
+
+// $passageiro1 = new Passageiro('João', 30);
+// $passageiro2 = new Passageiro('Maria', 25);
+
+
+// $passageiroRepository->salvar($passageiro1);
+// $passageiroRepository->salvar($passageiro2);
+
+// // Fechando a conexão com o banco de dados
+// $conexao->close();
 
 ?>
